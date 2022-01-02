b1f3c49cc25ba0461805878759031cb2bc365ecb
ixueaedu <ixueaedu@163.com>
2022-01-02 18:41:36 +0800

    3501 Change build gradle file.

--- MyCloudMusicAndroidJava/app/src/main/java/com/example/mycloudmusicandroidjava/MainActivity.java
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mycloudmusicandroidjava/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.mycloudmusicandroidjava;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
