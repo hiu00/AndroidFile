9f84ecf7bd8fdf607083f7874d5fa1eaa440a8b4
ixueaedu <ixueaedu@163.com>
2022-01-05 20:26:44 +0800

    3510 Add splash activity delay.

--- MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/SplashActivity.java
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/SplashActivity.java
@@ -1,16 +1,84 @@
 package com.example.mymusic.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
 
 import com.example.mymusic.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static final String TAG = "SplashActivity";
+
+    /**
+     * 下一步常量
+     */
+    private static final int MESSAGE_NEXT = 100;
+
+    /**
+     * 默认延时时间
+     */
+    private static final long DEFAULT_DELAY_TIME = 3000;
+
+    /**
+     * 创建Handler
+     */
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case MESSAGE_NEXT:
+                next();
+                break;
+            }
+        }
+    };
+
+    //下一步
+    private void next() {
+        Log.d(TAG,"MainActivity next");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //设置界面全屏
+
+        //获取decorView
+        View decorView = getWindow().getDecorView();
+
+        //判断版本
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) {
+            //11~18版本
+            decorView.setSystemUiVisibility(View.GONE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            //19及以上版本
+            //SYSTEM_UI_FLAG_HIDE_NAVIGATION:隐藏导航栏
+            //SYSTEM_UI_FLAG_IMMERSIVE_STICKY:从状态栏下拉会半透明悬浮显示一会儿状态栏和导航栏
+            //SYSTEM_UI_FLAG_FULLSCREEN:全屏
+            int options = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                    View.SYSTEM_UI_FLAG_FULLSCREEN;
+
+            //设置到控件
+            decorView.setSystemUiVisibility(options);
+        }
+
+        //延时3秒
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                handler.sendEmptyMessage(MESSAGE_NEXT);
+            }
+        },DEFAULT_DELAY_TIME);
     }
 }
