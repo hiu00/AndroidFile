0f3c5c7c5c4d5db05f9ea78cd79e52e9ef99f704
ixueaedu <ixueaedu@163.com>
2022-01-06 14:07:06 +0800

    3522 Add guide activity button click.

--- MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/GuideActivity.java
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/GuideActivity.java
@@ -1,7 +1,10 @@
 package com.example.mymusic.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +14,12 @@ import com.example.mymusic.R;
 /**
  * 引导界面
  */
-public class GuideActivity extends AppCompatActivity {
+public class GuideActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "GuideActivity";
+    private Button bt_login_or_register;
+    private Button bt_enter;
+
     /**
      *当前界面创建的时候
      * @param savedInstanceState
@@ -25,5 +33,38 @@ public class GuideActivity extends AppCompatActivity {
         //隐藏状态栏
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //找控件
+        //找登陆注册按钮
+        bt_login_or_register = findViewById(R.id.bt_login_or_register);
+        //找立即体验按钮
+        bt_enter = findViewById(R.id.bt_enter);
+
+        //设置点击事件
+        //登陆注册按钮
+        bt_login_or_register.setOnClickListener(this);
+        //立即体验按钮
+        bt_enter.setOnClickListener(this);
+    }
+
+    /**
+     * 点击回调事件
+     * @param view
+     */
+    @Override
+    public void onClick(View view) {
+        //因为有很多个按钮都设置同一个监听器
+        //所以这里需要区分到底是哪一个按钮点击
+        switch (view.getId()){
+            case R.id.bt_login_or_register:
+                //点击登陆注册按钮
+                Log.d(TAG,"onClick login or register");
+                break;
+            case R.id.bt_enter:
+                //点击立即体验按钮
+                Log.d(TAG,"onClick enter");
+                break;
+        }
+
     }
 }
