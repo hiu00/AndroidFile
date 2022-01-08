7bb585758e678ac58bc58e80b37a7416e10d0180
ixueaedu <ixueaedu@163.com>
2022-01-08 20:07:33 +0800

    3553 Add log or register click.

--- MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/LoginOrRegisterActivity.java
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/LoginOrRegisterActivity.java
@@ -3,17 +3,63 @@ package com.example.mymusic.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mymusic.R;
 
 /**
  * 登陆注册界面
  */
-public class LoginOrRegisterActivity extends AppCompatActivity {
+public class LoginOrRegisterActivity extends BaseCommonActivity implements View.OnClickListener {
+
+    private static final String TAG = "LoginOrRegisterActivity";
+    private Button bt_login;
+    private Button bt_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_or_register);
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+
+        //找登陆控件
+        bt_login = findViewById(R.id.bt_login);
+        //找注册控件
+        bt_register = findViewById(R.id.bt_register);
+    }
+
+    @Override
+    protected void initListeners() {
+        super.initListeners();
+        //设置登录按钮监听器
+        bt_login.setOnClickListener(this);
+        //设置注册按钮监听器
+        bt_register.setOnClickListener(this);
+    }
+
+    /**
+     * 按钮点击回调
+     *
+     * @param view
+     */
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bt_login:
+                //登录按钮点击
+                Log.d(TAG, "onClick: login");
+                break;
+            case R.id.bt_register:
+                //注册按钮点击
+                Log.d(TAG, "onClick: register");
+                break;
+        }
+
     }
 }
