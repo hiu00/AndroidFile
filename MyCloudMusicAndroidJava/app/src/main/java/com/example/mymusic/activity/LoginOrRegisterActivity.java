61d9a6f63a71a016431fc3558293bf03dbafabcc
ixueaedu <ixueaedu@163.com>
2022-01-08 20:13:55 +0800

    3554 Add login activity.

--- MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/LoginOrRegisterActivity.java
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/LoginOrRegisterActivity.java
@@ -54,6 +54,7 @@ public class LoginOrRegisterActivity extends BaseCommonActivity implements View.
             case R.id.bt_login:
                 //登录按钮点击
                 Log.d(TAG, "onClick: login");
+                startActivity(LoginActivity.class);
                 break;
             case R.id.bt_register:
                 //注册按钮点击
