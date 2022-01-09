a8bb4759dd09dc4f2a5f8893489f7e8118f1715d
ixueaedu <ixueaedu@163.com>
2022-01-09 15:40:25 +0800

    3563 Add tool bar.

--- MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/LoginActivity.java
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/LoginActivity.java
@@ -1,16 +1,33 @@
 package com.example.mymusic.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 import com.example.mymusic.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class LoginActivity extends BaseCommonActivity {
+
+    /**
+     * Toolbar
+     */
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+
+        //初始化toolbar
+        setSupportActionBar(toolbar);
     }
 }
