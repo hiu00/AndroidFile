6166a8ba5237a9526edf852d467df069fefda31e
ixueaedu <ixueaedu@163.com>
2022-01-09 16:39:51 +0800

    3565 Add back menu.

--- MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/BaseTitleActivity.java
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/BaseTitleActivity.java
@@ -1,5 +1,8 @@
 package com.example.mymusic.activity;
 
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.mymusic.R;
@@ -22,5 +25,33 @@ public class BaseTitleActivity extends BaseCommonActivity{
 
         //初始化toolbar
         setSupportActionBar(toolbar);
+
+        //是否显示返回按钮
+        if (isShowBackMenu()){
+            showBackMenu();
+        }
+    }
+
+    /**
+     * 显示返回按钮
+     */
+    private void showBackMenu() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    //是否显示返回按钮
+    private boolean isShowBackMenu() {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                //Toolbar返回按钮点击
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
