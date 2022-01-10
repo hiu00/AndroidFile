9b5ec22a817c9f91810481c51cbcd65c21ded83d
ixueaedu <ixueaedu@163.com>
2022-01-10 21:31:09 +0800

    3577 Add string util test.

---
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/util/StringUtil.java
@@ -0,0 +1,17 @@
+package com.example.mymusic.util;
+
+/**
+ * 字符串相关工具类
+ */
+public class StringUtil {
+    /**
+     * 是否是手机号
+     *
+     * @param value
+     * @return
+     */
+    public static boolean isPhone(String value){
+        //TODO 实现手机号格式判断
+        return false;
+    }
+}
---
+++ MyCloudMusicAndroidJava/app/src/test/java/com/example/mycloudmusicandroidjava/util/StringUtilTest.java
@@ -0,0 +1,32 @@
+package com.example.mycloudmusicandroidjava.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import com.example.mymusic.util.StringUtil;
+
+/**
+ * 字符串工具类测试
+ */
+public class StringUtilTest {
+    /**
+     * 测试是否是手机号
+     */
+    @Test
+    public void testIsPhone(){
+        //这是一个正确的手机号格式
+        //所以用断言判断结果为true
+        //只有结果为true才表示测试通过
+        //也就表示我们的代码没问题
+
+        //这里的assert其实是junit中的
+        assertTrue(StringUtil.isPhone("13141111111"));
+
+        //这是一个错误的手机号格式
+        //所以用断言判断结果为false
+        assertFalse(StringUtil.isPhone("123"));
+
+    }
+}
