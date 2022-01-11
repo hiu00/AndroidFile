1070d886cc0ac3adf61fa64cc114134123ce9093
ixueaedu <ixueaedu@163.com>
2022-01-11 22:11:21 +0800

    3600 Add sheet model.

---
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/domain/SheetDetailWrapper.java
@@ -0,0 +1,21 @@
+package com.example.mymusic.domain;
+
+/**
+ * 歌单详情包裹对象
+ *
+ * 只是用来测试
+ */
+public class SheetDetailWrapper {
+    /**
+     * 歌单对象
+     */
+    private Sheet data;
+
+    public Sheet getData() {
+        return data;
+    }
+
+    public void setData(Sheet data) {
+        this.data = data;
+    }
+}
