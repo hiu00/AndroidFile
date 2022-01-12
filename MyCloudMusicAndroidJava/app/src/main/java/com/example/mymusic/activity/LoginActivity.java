5ab1ae85267a1a55504f81c7ab317ac31ff2697b
ixueaedu <ixueaedu@163.com>
2022-01-12 18:46:37 +0800

    4650 Add api.

--- MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/LoginActivity.java
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/LoginActivity.java
@@ -13,6 +13,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.mymusic.R;
+import com.example.mymusic.api.Api;
 import com.example.mymusic.api.Service;
 import com.example.mymusic.domain.SheetDetailWrapper;
 import com.example.mymusic.util.Constant;
@@ -69,86 +70,112 @@ public class LoginActivity extends BaseTitleActivity {
         //请求歌单详情
         //用来测试网络请求框架
 
-        //OkHttp有很多配置
-        OkHttpClient.Builder OkHttpClientBuilder=new OkHttpClient.Builder();
+//        //初始化OkHttp
+//        OkHttpClient.Builder OkHttpClientBuilder=new OkHttpClient.Builder();
+//
+//        //创建一个Retrofit
+//        Retrofit retrofit=new Retrofit.Builder()
+//                //让Retrofit使用OkHttp请求网络
+//                .client(OkHttpClientBuilder.build())
+//
+//                //这里就是配置API地址
+//                .baseUrl(Constant.ENDPOINT)
+//
+//                //适配RxJava
+//                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+//
+//                //自动JSON转换
+//                //包括请求参数和相应
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build();
+//
+//        //创建一个service
+//        Service service = retrofit.create(Service.class);
 
-        //创建一个Retrofit
-        Retrofit retrofit=new Retrofit.Builder()
-                //让Retrofit使用OkHttp请求网络
-                .client(OkHttpClientBuilder.build())
+//        //请求歌单详情
+//        service.sheetDetail("1")
+//                //在什么线程里去执行这个任务（子线程）
+//                .subscribeOn(Schedulers.io())
+//
+//                //在哪里去观察（主线程）
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Observer<SheetDetailWrapper>() {
+//                    @Override
+//                    public void onSubscribe(@NonNull Disposable d) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onNext(@NonNull SheetDetailWrapper sheetDetailWrapper) {
+//                        //请求成功
+//                        LogUtil.d(TAG,"request sheet detail success:"+sheetDetailWrapper.getData().getTitle());
+//
+//                    }
+//
+//                    @Override
+//                    public void onError(@NonNull Throwable e) {
+//                        //请求失败
+//                        LogUtil.d(TAG,"request sheet detail failed:"+e.getLocalizedMessage());
+//
+//                        //判断错误类型
+//                        if (e instanceof UnknownHostException){
+//                            ToastUtil.errorShortToast(R.string.error_network_unknow_host);
+//                        }else if (e instanceof ConnectException){
+//                            ToastUtil.errorShortToast(R.string.error_network_connect);
+//                        }else if (e instanceof SocketTimeoutException){
+//                            ToastUtil.errorShortToast(R.string.error_network_timeout);
+//                        }else if (e instanceof HttpException){
+//                            HttpException exception= (HttpException) e;
+//
+//                            //获取响应码
+//                            int code=exception.code();
+//                            if (code==401){
+//                                ToastUtil.errorShortToast(R.string.error_network_not_auth);
+//                            }else if (code == 403) {
+//                                ToastUtil.errorShortToast(R.string.error_network_not_permission);
+//                            } else if (code == 404) {
+//                                ToastUtil.errorShortToast(R.string.error_network_not_found);
+//                            } else if (code >= 500) {
+//                                ToastUtil.errorShortToast(R.string.error_network_server);
+//                            } else {
+//                                ToastUtil.errorShortToast(R.string.error_network_unknown);
+//                            }
+//                        } else{
+//                            ToastUtil.errorShortToast(R.string.error_network_unknown);
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onComplete() {
+//
+//                    }
+//                });
 
-                //这里就是配置API地址
-                .baseUrl(Constant.ENDPOINT)
-
-                //适配RxJava
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-
-                //自动JSON转换
-                //包括请求参数和相应
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        //创建一个service
-        Service service = retrofit.create(Service.class);
-
-        //请求歌单详情
-        service.sheetDetail("1")
-                //在什么线程里去执行这个任务（子线程）
-                .subscribeOn(Schedulers.io())
-
-                //在哪里去观察（主线程）
-                .observeOn(AndroidSchedulers.mainThread())
+        //使用重构后的Api
+        Api.getInstance()
+                .sheetDetail("1")
                 .subscribe(new Observer<SheetDetailWrapper>() {
-                    @Override
-                    public void onSubscribe(@NonNull Disposable d) {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
 
-                    }
+            }
 
-                    @Override
-                    public void onNext(@NonNull SheetDetailWrapper sheetDetailWrapper) {
-                        //请求成功
-                        LogUtil.d(TAG,"request sheet detail success:"+sheetDetailWrapper.getData().getTitle());
+            @Override
+            public void onNext(@NonNull SheetDetailWrapper sheetDetailWrapper) {
+                //请求成功
+                LogUtil.d(TAG,"request sheet detail success:"+sheetDetailWrapper.getData().getTitle());
+            }
 
-                    }
+            @Override
+            public void onError(@NonNull Throwable e) {
 
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-                        //请求失败
-                        LogUtil.d(TAG,"request sheet detail failed:"+e.getLocalizedMessage());
+            }
 
-                        //判断错误类型
-                        if (e instanceof UnknownHostException){
-                            ToastUtil.errorShortToast(R.string.error_network_unknow_host);
-                        }else if (e instanceof ConnectException){
-                            ToastUtil.errorShortToast(R.string.error_network_connect);
-                        }else if (e instanceof SocketTimeoutException){
-                            ToastUtil.errorShortToast(R.string.error_network_timeout);
-                        }else if (e instanceof HttpException){
-                            HttpException exception= (HttpException) e;
+            @Override
+            public void onComplete() {
 
-                            //获取响应码
-                            int code=exception.code();
-                            if (code==401){
-                                ToastUtil.errorShortToast(R.string.error_network_not_auth);
-                            }else if (code == 403) {
-                                ToastUtil.errorShortToast(R.string.error_network_not_permission);
-                            } else if (code == 404) {
-                                ToastUtil.errorShortToast(R.string.error_network_not_found);
-                            } else if (code >= 500) {
-                                ToastUtil.errorShortToast(R.string.error_network_server);
-                            } else {
-                                ToastUtil.errorShortToast(R.string.error_network_unknown);
-                            }
-                        } else{
-                            ToastUtil.errorShortToast(R.string.error_network_unknown);
-                        }
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-                });
+            }
+        });
 
 //        //获取用户名
 //        String username = et_username.getText().toString().trim();
---
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/api/Api.java
@@ -0,0 +1,80 @@
+package com.example.mymusic.api;
+
+import com.example.mymusic.domain.SheetDetailWrapper;
+import com.example.mymusic.util.Constant;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * 网络请求接口包装类
+ * 对外部提供一个和框架无关的接口
+ */
+public class Api {
+    /**
+     * 实例
+     */
+    private static Api instance;
+
+    /**
+     * Service单例
+     */
+    private final Service service;
+
+    /**
+     * 返回当前对象的唯一实例
+     *
+     * 单例设计模式
+     * 由于移动端很少有高并发
+     * 所以这个就是简单判断
+     *
+     * @return
+     */
+    public static Api getInstance(){
+        if(instance==null){
+            instance=new Api();
+        }
+        return instance;
+    }
+
+    public Api() {
+        //初始化OkHttp
+        OkHttpClient.Builder OkHttpClientBuilder=new OkHttpClient.Builder();
+
+        //创建一个Retrofit
+        Retrofit retrofit=new Retrofit.Builder()
+                //让Retrofit使用OkHttp请求网络
+                .client(OkHttpClientBuilder.build())
+
+                //这里就是配置API地址
+                .baseUrl(Constant.ENDPOINT)
+
+                //适配RxJava
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+
+                //自动JSON转换
+                //包括请求参数和相应
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        //创建一个service
+        service = retrofit.create(Service.class);
+    }
+
+    /**
+     * 歌单详情
+     *
+     * @param id
+     * @return
+     */
+    public Observable<SheetDetailWrapper> sheetDetail(String id) {
+        return service.sheetDetail(id)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+}
