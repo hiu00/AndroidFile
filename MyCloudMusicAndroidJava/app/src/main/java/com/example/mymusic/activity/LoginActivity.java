dabc4646a2b471479fa342bd1843f8cbfeeb44d5
ixueaedu <ixueaedu@163.com>
2022-01-12 15:47:03 +0800

    3591 Add request sheet detail.

--- MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/LoginActivity.java
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.mymusic.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -12,6 +13,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.mymusic.R;
+import com.example.mymusic.api.Service;
+import com.example.mymusic.domain.SheetDetailWrapper;
+import com.example.mymusic.util.Constant;
 import com.example.mymusic.util.LogUtil;
 import com.example.mymusic.util.StringUtil;
 import com.example.mymusic.util.ToastUtil;
@@ -21,6 +25,14 @@ import org.apache.commons.lang3.StringUtils;
 import butterknife.BindView;
 import butterknife.OnClick;
 import es.dmoral.toasty.Toasty;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends BaseTitleActivity {
     private static final String TAG = "LoginActivity";
@@ -49,43 +61,101 @@ public class LoginActivity extends BaseTitleActivity {
     public void onLoginClick(Button view) {
         LogUtil.d(TAG, "onLoginClick");
 
-        //获取用户名
-        String username = et_username.getText().toString().trim();
-        if (StringUtils.isBlank(username)){
-            LogUtil.w(TAG,"onLoginClick user empty");
-            //Toast.makeText(getMainActivity(), R.string.enter_username, Toast.LENGTH_SHORT).show();
-            //Toasty.error(getMainActivity(),R.string.enter_username,Toasty.LENGTH_SHORT).show();
+        //请求歌单详情
+        //用来测试网络请求框架
 
-            ToastUtil.errorShortToast(R.string.enter_username);
-            return;
-        }
+        //OkHttp有很多配置
+        OkHttpClient.Builder OkHttpClientBuilder=new OkHttpClient.Builder();
 
-        //判断用户名格式
-        //如果用户名
-        //不是手机号也不是邮箱
-        //就是格式错误
-        if (!(StringUtil.isPhone(username)||StringUtil.isEmail(username))){
-            ToastUtil.errorShortToast(R.string.error_username_format);
-            return;
-        }
-        //获取密码
-        String password = et_password.getText().toString().trim();
-        if (TextUtils.isEmpty(password)){
-            LogUtil.w(TAG,"onLoginClick password empty");
-            //Toast.makeText(getMainActivity(), R.string.enter_password, Toast.LENGTH_SHORT).show();
+        //创建一个Retrofit
+        Retrofit retrofit=new Retrofit.Builder()
+                //让Retrofit使用OkHttp请求网络
+                .client(OkHttpClientBuilder.build())
 
-            ToastUtil.errorLongToast(R.string.enter_password);
-            return;
-        }
+                //这里就是配置API地址
+                .baseUrl(Constant.ENDPOINT)
 
-        //判断密码格式
-        if(!(StringUtil.isPassword(password))){
-            ToastUtil.errorShortToast(R.string.error_password_format);
-            return;
-        }
+                //适配RxJava
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
 
-        //TODO 调用登陆方法
-        ToastUtil.successLongToast(R.string.login_sucess);
+                //自动JSON转换
+                //包括请求参数和相应
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        //创建一个service
+        Service service = retrofit.create(Service.class);
+
+        //请求歌单详情
+        service.sheetDetail("1")
+                //在什么线程里去执行这个任务（子线程）
+                .subscribeOn(Schedulers.io())
+
+                //在哪里去观察（主线程）
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<SheetDetailWrapper>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull SheetDetailWrapper sheetDetailWrapper) {
+                        //请求成功
+                        LogUtil.d(TAG,"request sheet detail success:"+sheetDetailWrapper.getData().getTitle());
+
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        //请求失败
+                        LogUtil.d(TAG,"request sheet detail failed:"+e.getLocalizedMessage());
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+//        //获取用户名
+//        String username = et_username.getText().toString().trim();
+//        if (StringUtils.isBlank(username)){
+//            LogUtil.w(TAG,"onLoginClick user empty");
+//            //Toast.makeText(getMainActivity(), R.string.enter_username, Toast.LENGTH_SHORT).show();
+//            //Toasty.error(getMainActivity(),R.string.enter_username,Toasty.LENGTH_SHORT).show();
+//
+//            ToastUtil.errorShortToast(R.string.enter_username);
+//            return;
+//        }
+//
+//        //判断用户名格式
+//        //如果用户名
+//        //不是手机号也不是邮箱
+//        //就是格式错误
+//        if (!(StringUtil.isPhone(username)||StringUtil.isEmail(username))){
+//            ToastUtil.errorShortToast(R.string.error_username_format);
+//            return;
+//        }
+//        //获取密码
+//        String password = et_password.getText().toString().trim();
+//        if (TextUtils.isEmpty(password)){
+//            LogUtil.w(TAG,"onLoginClick password empty");
+//            //Toast.makeText(getMainActivity(), R.string.enter_password, Toast.LENGTH_SHORT).show();
+//
+//            ToastUtil.errorLongToast(R.string.enter_password);
+//            return;
+//        }
+//
+//        //判断密码格式
+//        if(!(StringUtil.isPassword(password))){
+//            ToastUtil.errorShortToast(R.string.error_password_format);
+//            return;
+//        }
+//
+//        //TODO 调用登陆方法
+//        ToastUtil.successLongToast(R.string.login_sucess);
     }
 
     /**
