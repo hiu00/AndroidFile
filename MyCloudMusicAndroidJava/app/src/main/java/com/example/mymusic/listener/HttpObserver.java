643d07e205cf7e9355d51b6a8306bf05da924d2f
ixueaedu <ixueaedu@163.com>
2022-01-13 22:15:12 +0800

    3614 Move http handler method to http util.

--- MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/listener/HttpObserver.java
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/listener/HttpObserver.java
@@ -1,20 +1,12 @@
 package com.example.mymusic.listener;
 
-import android.text.TextUtils;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
-import com.example.mymusic.R;
 import com.example.mymusic.domain.response.BaseResponse;
+import com.example.mymusic.util.HttpUtil;
 import com.example.mymusic.util.LogUtil;
-import com.example.mymusic.util.ToastUtil;
-
-import java.net.ConnectException;
-import java.net.SocketTimeoutException;
-import java.net.UnknownHostException;
-
-import retrofit2.HttpException;
 
 /**
  * 网络请求Observer
@@ -49,7 +41,7 @@ public abstract class HttpObserver<T> extends ObserverAdapter<T>{
         }else {
             //有状态码
             //表示请求出错了
-            requestErrorHandler(t, null);
+            handlerRequest(t, null);
         }
 
 
@@ -79,7 +71,7 @@ public abstract class HttpObserver<T> extends ObserverAdapter<T>{
      * @param t     请求返回的对象
      * @param e 错误信息
      */
-    private void requestErrorHandler(T t, Throwable e) {
+    private void handlerRequest(T t, Throwable e) {
         if (onFailed(t,e)){
             //回调了请求失败方法
             //并且该方法返回了true
@@ -88,47 +80,7 @@ public abstract class HttpObserver<T> extends ObserverAdapter<T>{
             //那我们框架内部就不用做任何事情了
         }
         else {
-            if (e!=null){
-                //判断错误类型
-                if (e instanceof UnknownHostException){
-                    ToastUtil.errorShortToast(R.string.error_network_unknow_host);
-                }else if (e instanceof ConnectException){
-                    ToastUtil.errorShortToast(R.string.error_network_connect);
-                }else if (e instanceof SocketTimeoutException){
-                    ToastUtil.errorShortToast(R.string.error_network_timeout);
-                }else if (e instanceof HttpException){
-                    HttpException exception= (HttpException) e;
-
-                    //获取响应码
-                    int code=exception.code();
-                    if (code==401){
-                        ToastUtil.errorShortToast(R.string.error_network_not_auth);
-                    }else if (code == 403) {
-                        ToastUtil.errorShortToast(R.string.error_network_not_permission);
-                    } else if (code == 404) {
-                        ToastUtil.errorShortToast(R.string.error_network_not_found);
-                    } else if (code >= 500) {
-                        ToastUtil.errorShortToast(R.string.error_network_server);
-                    } else {
-                        ToastUtil.errorShortToast(R.string.error_network_unknown);
-                    }
-                } else{
-                    ToastUtil.errorShortToast(R.string.error_network_unknown);
-                }
-
-            }else {
-                if (t instanceof BaseResponse) {//instanceof用作对象的判断
-                    //判断具体的业务请求
-                    BaseResponse baseResponse = (BaseResponse) t;
-                    if (TextUtils.isEmpty(baseResponse.getMessage())){
-                        //没有错误提示信息
-                        ToastUtil.errorShortToast(R.string.error_network_unknown);
-                    }else {
-                        //有错误提示信息
-                        ToastUtil.errorShortToast(baseResponse.getMessage());
-                    }
-                }
-            }
+            HttpUtil.handlerRequest(t,e);
         }
     }
 
@@ -138,6 +90,6 @@ public abstract class HttpObserver<T> extends ObserverAdapter<T>{
         Log.d(TAG, "onError: "+e.getLocalizedMessage());
 
         //处理错误
-        requestErrorHandler(null,e);
+        handlerRequest(null,e);
     }
 }
---
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/util/HttpUtil.java
@@ -0,0 +1,67 @@
+package com.example.mymusic.util;
+
+import android.text.TextUtils;
+
+import com.example.mymusic.R;
+import com.example.mymusic.domain.response.BaseResponse;
+
+import java.net.ConnectException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+
+import retrofit2.HttpException;
+
+/**
+ * 网络请求相关的辅助方法
+ */
+public class HttpUtil {
+    /**
+     * 网络请求错误处理
+     *
+     * @param t
+     * @param e
+     */
+    public static void handlerRequest(Object t, Throwable e) {
+        if (e!=null){
+            //判断错误类型
+            if (e instanceof UnknownHostException){
+                ToastUtil.errorShortToast(R.string.error_network_unknow_host);
+            }else if (e instanceof ConnectException){
+                ToastUtil.errorShortToast(R.string.error_network_connect);
+            }else if (e instanceof SocketTimeoutException){
+                ToastUtil.errorShortToast(R.string.error_network_timeout);
+            }else if (e instanceof HttpException){
+                HttpException exception= (HttpException) e;
+
+                //获取响应码
+                int code=exception.code();
+                if (code==401){
+                    ToastUtil.errorShortToast(R.string.error_network_not_auth);
+                }else if (code == 403) {
+                    ToastUtil.errorShortToast(R.string.error_network_not_permission);
+                } else if (code == 404) {
+                    ToastUtil.errorShortToast(R.string.error_network_not_found);
+                } else if (code >= 500) {
+                    ToastUtil.errorShortToast(R.string.error_network_server);
+                } else {
+                    ToastUtil.errorShortToast(R.string.error_network_unknown);
+                }
+            } else{
+                ToastUtil.errorShortToast(R.string.error_network_unknown);
+            }
+
+        }else {
+            if (t instanceof BaseResponse) {//instanceof用作对象的判断
+                //判断具体的业务请求
+                BaseResponse baseResponse = (BaseResponse) t;
+                if (TextUtils.isEmpty(baseResponse.getMessage())){
+                    //没有错误提示信息
+                    ToastUtil.errorShortToast(R.string.error_network_unknown);
+                }else {
+                    //有错误提示信息
+                    ToastUtil.errorShortToast(baseResponse.getMessage());
+                }
+            }
+        }
+    }
+}
