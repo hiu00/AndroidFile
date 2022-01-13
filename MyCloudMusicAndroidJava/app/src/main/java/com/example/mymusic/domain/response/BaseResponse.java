82ae0055fe3f391872153954875e44dd71e9d2bb
ixueaedu <ixueaedu@163.com>
2022-01-13 15:59:29 +0800

    3609 Add base response.

---
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/domain/response/BaseResponse.java
@@ -0,0 +1,38 @@
+package com.example.mymusic.domain.response;
+
+/**
+ * 通用网络请求响应模型
+ */
+
+public class BaseResponse {
+
+    /**
+     * 状态码
+     *
+     * 只有发生了错误才会有值
+     */
+    private Integer status;
+
+    /**
+     * 出错的提示信息
+     *
+     * 发生了错误不一定有
+     */
+    private String message;
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
