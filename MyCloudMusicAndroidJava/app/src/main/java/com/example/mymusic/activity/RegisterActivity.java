d948410e989e93439d8cb9c965c2cd3c42ab4d37
ixueaedu <ixueaedu@163.com>
2022-01-15 13:32:50 +0800

    3635 Add register layout.

--- MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/RegisterActivity.java
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/RegisterActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 import com.example.mymusic.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends BaseTitleActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
