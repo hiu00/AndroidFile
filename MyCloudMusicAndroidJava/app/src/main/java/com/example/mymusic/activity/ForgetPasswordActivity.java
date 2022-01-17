414ea9416ea240e7f3e3ac96fb12537fa9fae834
ixueaedu <ixueaedu@163.com>
2022-01-17 14:42:12 +0800

    3669 Add forget password activity.

---
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/ForgetPasswordActivity.java
@@ -0,0 +1,19 @@
+package com.example.mymusic.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mymusic.R;
+
+/**
+ * 忘记密码界面
+ */
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
