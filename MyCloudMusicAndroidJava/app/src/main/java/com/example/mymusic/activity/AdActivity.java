d8c888549f82250b604492911c1b34c44774e777
ixueaedu <ixueaedu@163.com>
2022-01-19 14:48:54 +0800

    4661 Add show ad.

--- MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/AdActivity.java
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/AdActivity.java
@@ -3,8 +3,10 @@ package com.example.mymusic.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.Button;
 
+import com.example.mymusic.MainActivity;
 import com.example.mymusic.R;
 import com.example.mymusic.util.LogUtil;
 
@@ -22,6 +24,7 @@ public class AdActivity extends BaseCommonActivity {
      */
     @BindView(R.id.bt_skip_ad)
     Button bt_skip_ad;
+    private CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +41,49 @@ public class AdActivity extends BaseCommonActivity {
     }
 
     /**
+     * 界面以及显示出来了调用
+     * 从后台切换到前台还会调用
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //倒计时的总时间,间隔
+        //单位为毫秒
+        countDownTimer = new CountDownTimer(5000, 1000) {
+            /**
+             * 每次间隔调用
+             * @param l
+             */
+            @Override
+            public void onTick(long l) {
+                bt_skip_ad.setText(getString(R.string.count_second,l/1000));
+
+            }
+
+            /**
+             * 倒计时完成
+             */
+            @Override
+            public void onFinish() {
+                //倒计时完成
+                //执行下一步
+                next();
+            }
+        };
+
+        //启动定时器
+        countDownTimer.start();
+    }
+
+    /**
+     * 跳转到主页面
+     */
+    private void next() {
+        startActivoityAfterFinishThis(MainActivity.class);
+    }
+
+    /**
      * 广告点击了
      */
     @OnClick(R.id.bt_ad)
@@ -51,5 +97,30 @@ public class AdActivity extends BaseCommonActivity {
     @OnClick(R.id.bt_skip_ad)
     public void onSkipAd() {
         LogUtil.d(TAG, "onSkipAd");
+
+        //取消倒计时
+        cancelCountDown();
+
+        //跳转到首页
+        next();
+    }
+
+    /**
+     * 销毁定时器
+     */
+    @Override
+    protected void onDestroy() {
+        cancelCountDown();
+
+        super.onDestroy();
+    }
+
+    /**
+     * 取消倒计时
+     */
+    private void cancelCountDown() {
+        if (countDownTimer!=null){
+            countDownTimer.cancel();
+        }
     }
 }
