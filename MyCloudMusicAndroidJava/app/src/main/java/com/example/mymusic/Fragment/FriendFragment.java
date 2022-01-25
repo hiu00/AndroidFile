7c1385b97de4a28f4e53e48177c1fc4023d794a3
ixueaedu <ixueaedu@163.com>
2022-01-25 22:13:35 +0800

    3716 Add main four fragment.

---
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/Fragment/FriendFragment.java
@@ -0,0 +1,43 @@
+package com.example.mymusic.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.mymusic.R;
+
+/**
+ * 首页-朋友 界面
+ */
+public class FriendFragment extends BaseCommonFragment{
+
+    /**
+     * 构造方法
+     * 固定写法
+     * @return
+     */
+    public static FriendFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        FriendFragment fragment = new FriendFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    /**
+     * 返回布局文件
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+    @Override
+    protected View getLayoutView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_friend,null);
+    }
+}
