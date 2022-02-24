195b0073b9c9c7c76c5e781c4961e704280ea717
ixueaedu <ixueaedu@163.com>
2022-02-24 21:24:48 +0800

    3753 Add show sheet data.

--- MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/SheetDetailActivity.java
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/SheetDetailActivity.java
@@ -8,7 +8,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.mymusic.R;
 import com.example.mymusic.adapter.SongAdapter;
@@ -17,7 +20,10 @@ import com.example.mymusic.domain.Sheet;
 import com.example.mymusic.domain.response.DetailResponse;
 import com.example.mymusic.listener.HttpObserver;
 import com.example.mymusic.util.Constant;
+import com.example.mymusic.util.ImageUtil;
 import com.example.mymusic.util.LogUtil;
+
+import org.apache.commons.lang3.StringUtils;
 
 import butterknife.BindView;
 
@@ -40,6 +46,16 @@ public class SheetDetailActivity extends BaseTitleActivity {
      */
     private Sheet data;
     private SongAdapter adapter;
+    private View ll_header;
+    private ImageView iv_banner;
+    private TextView tv_title;
+    private ImageView iv_avatar;
+    private TextView tv_nickname;
+    private View ll_comment_container;
+    private TextView tv_comment_count;
+    private Button bt_collection;
+    private View ll_play_all_container;
+    private TextView tv_count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +82,36 @@ public class SheetDetailActivity extends BaseTitleActivity {
     private View createHeaderView(){
         //从XML创建View
         View view = getLayoutInflater().inflate(R.layout.header_sheet_detail, (ViewGroup) rv.getParent(), false);
+
+        //头部容器
+        ll_header = view.findViewById(R.id.ll_header);
+
+        //封面图
+        iv_banner = view.findViewById(R.id.iv_banner);
+
+        //标题
+        tv_title = view.findViewById(R.id.tv_title);
+
+        //歌单创建者头像
+        iv_avatar = view.findViewById(R.id.iv_avatar);
+
+        //歌单创建者昵称
+        tv_nickname = view.findViewById(R.id.tv_nickname);
+
+        //评论容器
+        ll_comment_container = view.findViewById(R.id.ll_comment_container);
+
+        //评论数
+        tv_comment_count = view.findViewById(R.id.tv_comment_count);
+
+        //收藏按钮
+        bt_collection = view.findViewById(R.id.bt_collection);
+
+        //播放全部容器
+        ll_play_all_container = view.findViewById(R.id.ll_play_all_container);
+
+        //歌曲数
+        tv_count = view.findViewById(R.id.tv_count);
 
         //返回View
         return view;
@@ -122,6 +168,33 @@ public class SheetDetailActivity extends BaseTitleActivity {
             //设置数据
             adapter.replaceData(data.getSongs());
         }
+
+        if (StringUtils.isBlank(data.getBanner())) {
+            //如果图片为空
+
+            //就用默认图片
+            iv_banner.setImageResource(R.drawable.placeholder);
+        } else {
+            //有图片
+
+            ImageUtil.show(getMainActivity(), iv_banner, data.getBanner());
+        }
+
+        //显示标题
+        tv_title.setText(data.getTitle());
+
+        //头像
+        ImageUtil.showAvatar(getMainActivity(), iv_avatar, data.getUser().getAvatar());
+
+        //昵称
+        tv_nickname.setText(data.getUser().getNickname());
+
+        //评论数
+        tv_comment_count.setText(String.valueOf(data.getComments_count()));
+
+        //音乐数
+        tv_count.setText(getResources().getString(R.string.music_count, data.getSongs_count()));
+
 
     }
 
