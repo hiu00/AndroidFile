ff5feaddb679b55b5c705037ffb8e4ad790d8797
ixueaedu <ixueaedu@163.com>
2022-03-16 16:27:52 +0800

    3881 Add show music basic info.

--- MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/MusicPlayerActivity.java
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/activity/MusicPlayerActivity.java
@@ -5,13 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.mymusic.R;
+import com.example.mymusic.domain.Song;
+import com.example.mymusic.listener.ListManager;
+import com.example.mymusic.manager.MusicPlayerManager;
+import com.example.mymusic.service.MusicPlayerService;
+import com.example.mymusic.util.ImageUtil;
+
+import butterknife.BindView;
 
 /**
  * 黑胶唱片界面
  */
 public class MusicPlayerActivity extends BaseTitleActivity {
+    /**
+     * 背景
+     */
+    @BindView(R.id.iv_background)
+    ImageView iv_background;
+    private ListManager listManager;
+    private MusicPlayerManager musicPlayerManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +49,44 @@ public class MusicPlayerActivity extends BaseTitleActivity {
 
         //显示亮色状态栏
         lightStatusBar();
+    }
+
+    @Override
+    protected void initDatum() {
+        super.initDatum();
+
+        //初始化列表管理器
+        listManager = MusicPlayerService.getListManager(getApplicationContext());
+
+        //初始化播放管理器
+        musicPlayerManager = MusicPlayerService.getMusicPlayerManager(getApplicationContext());
+    }
+
+    /**
+     * 显示初始化数据
+     */
+    public void showInitData(){
+        //获取当前播放的音乐
+        Song data = listManager.getData();
+
+        //设置标题
+        setTitle(data.getTitle());
+
+        //设置子标题
+        toolbar.setSubtitle(data.getSinger().getNickname());
+
+        //显示背景
+        ImageUtil.show(getMainActivity(),iv_background,data.getBanner());
+    }
+
+    /**
+     * 界面显示了
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //显示初始化数据
+        showInitData();
     }
 }
