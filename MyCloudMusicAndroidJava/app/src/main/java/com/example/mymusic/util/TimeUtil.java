b391de02991a326e3576ae649c847f1dcf098bd9
ixueaedu <ixueaedu@163.com>
2022-03-18 19:57:53 +0800

    3914 Add lrc parse.

--- MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/util/TimeUtil.java
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/util/TimeUtil.java
@@ -6,13 +6,17 @@ import java.io.Serializable;
  * 日期时间格式化
  */
 public class TimeUtil {
+
+    private static String data;
+
     /**
      * 将毫秒格式化为 分：秒
+     *
      * @param data
      * @return
      */
     public static String formatMinuteSecond(int data) {
-        if (data==0){
+        if (data == 0) {
             return "00:00";
         }
 
@@ -20,11 +24,33 @@ public class TimeUtil {
         data /= 1000;
 
         //计算分钟
-        int minute=data/60;
+        int minute = data / 60;
 
         //秒
-        int second = data-(minute*60);
+        int second = data - (minute * 60);
 
-        return String.format("%02d:%02d",minute,second);
+        return String.format("%02d:%02d", minute, second);
+    }
+
+    /**
+     * 将分秒毫秒数据转换为毫秒
+     *
+     * @param data 格式为00：00.000
+     * @return
+     */
+    public static long parseToInt(String data) {
+        //将：替换成.号
+        data = data.replace(":", ".");
+
+        //使用.拆分
+        String[] strings = data.split("\\.");
+
+        //分别取出分，秒，毫秒
+        int m = Integer.parseInt(strings[0]);
+        int s = Integer.parseInt(strings[1]);
+        int ms = Integer.parseInt(strings[2]);
+
+        //统一转为毫秒
+        return (m * 60 + s) * 1000 + ms;
     }
 }
--- MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/util/lyric/LRCLyricParser.java
+++ MyCloudMusicAndroidJava/app/src/main/java/com/example/mymusic/util/lyric/LRCLyricParser.java
@@ -1,6 +1,11 @@
 package com.example.mymusic.util.lyric;
 
+import com.example.mymusic.domain.Lyric.Line;
 import com.example.mymusic.domain.Lyric.Lyric;
+import com.example.mymusic.util.TimeUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * LRC歌词解析器
@@ -15,7 +20,59 @@ public class LRCLyricParser {
         //创建歌词解析后的对象
         Lyric result = new Lyric();
 
+        //初始化一个数组
+        List<Line> datum = new ArrayList<>();
+
+        //使用\n拆分歌词
+        String[] strings = data.split("\n");
+
+        //循环每一行歌词
+        for (String lineString : strings
+        ) {
+            //解析每一行歌词
+            Line line = parserLine(lineString);
+            if (line != null) {
+                //过滤了空行歌词
+                datum.add(line);
+            }
+        }
+
+        //将歌词行设置到歌词对象
+        result.setDatum(datum);
+
         //返回解析后的歌词
         return result;
+    }
+
+    /**
+     * 解析一行歌词
+     * 例如：[00:00.300]爱的代价 - 李宗盛
+     *
+     * @param data
+     * @return
+     */
+    private static Line parserLine(String data) {
+        //过滤元数据
+        //由于这里用不到所以过滤了
+        if (data.startsWith("[0")) {
+            //创建歌词行
+            Line line = new Line();
+
+            //去处前面的[
+            data = data.substring(1);
+
+            //使用]拆分
+            String[] commands = data.split("]", -1);
+
+            //开始时间
+            line.setStartTime(TimeUtil.parseToInt(commands[0]));
+
+            //歌词
+            line.setData(commands[1]);
+
+            //返回解析后的这行歌词
+            return line;
+        }
+        return null;
     }
 }
